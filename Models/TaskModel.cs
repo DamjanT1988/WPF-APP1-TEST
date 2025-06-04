63e5c3f4817815784736242e62e6f9d47e4a1486
DamjanT1988 <Damjan_t@hotmail.com>
2025-06-04 21:01:32 +0200

    Add project files.

---
+++ Models/TaskModel.cs
@@ -0,0 +1,5 @@
+﻿public class TaskModel
+{
+    public string Title { get; set; }
+    public bool IsDone { get; set; }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<TaskModel> Tasks { get; set; } = new();
+
+    private string _newTaskTitle;
+    public string NewTaskTitle
+    {
+        get => _newTaskTitle;
+        set { _newTaskTitle = value; OnPropertyChanged(); }
+    }
+
+    public ICommand AddTaskCommand { get; }
+
+    public MainViewModel()
+    {
+        AddTaskCommand = new RelayCommand(AddTask, () => !string.IsNullOrWhiteSpace(NewTaskTitle));
+    }
+
+    private void AddTask()
+    {
+        Tasks.Add(new TaskModel { Title = NewTaskTitle });
+        NewTaskTitle = string.Empty;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
